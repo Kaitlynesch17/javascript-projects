65925f2e63b85149ba63c78e32b2680a2ca99329
Kaitlynesch17 <Kaitlyn.esch@gmail.com>
2024-07-08 18:55:46 -0500

    studio complete

--- objects-and-math/studio/ObjectsStudio03.js
+++ objects-and-math/studio/ObjectsStudio03.js
@@ -1,8 +1,34 @@
 // Code your crewMass function here:
 
+function crewMass(crew){
+  let totalMass = 0;
+for (let i = 0; i < crew.length; i++) {
+  totalMass = totalMass + crew[i].mass;
+}
+  return Math.round(totalMass * 100) / 10;
+};
 
 // Code your fuelRequired function here:
+const rocketMass = 75000;
 
+function fuelRequired(rocketMass, crew){
+let mass = rocketMass + crewMass(crew);
+
+
+let safety = 0; 
+
+for (let i = 0; i < crew.length; i++){
+  if (crew[i].species.toLowerCase() === "dog" || crew[i].species.toLowerCase() === "cat") {
+    safety = safety + 200;
+  } else {
+    safety = safety + 100;
+  }
+}
+
+let fuelNeeded = (mass * 9.5) + safety;
+
+  return Math.ceil(fuelNeeded);
+};
 
 // The pre-selected crew is in the array at the end of this file.
 // Feel free to add, remove, or switch crew members as you see fit.
@@ -52,3 +78,4 @@ let candidateA = {
  
  let crew = [candidateB,candidateD,candidateF];
  
+ console.log(`The mission has a launch mass of ${crewMass(crew) + rocketMass} kg and requires ${fuelRequired(rocketMass, crew)} kg of fuel`);
