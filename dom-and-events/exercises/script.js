5c6a1bdc698aa8b1f48d3aa0fc60486a828fae7c
Kaitlynesch17 <Kaitlyn.esch@gmail.com>
2024-08-05 18:47:57 -0500

    DOM Complete

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,6 +4,22 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+    });
+
+    missionAbort.addEventListener('mouseover', function (event) {
+        event.target.style.backgroundColor = "red";
+    });
+
+    missionAbort.addEventListener('mouseout', function (event) {
+        event.target.style.backgroundColor = "";
+    });
+
+    missionAbort.addEventListener('click', function(event) {
+        window.confirm("Are you sure you want to abort the mission?");
+        window.alert("Mission aborted! Space shuttle returning home");
+    })
     
 }
 
